eb794ec2d953e25d4389ad1cf9a94f11d1cf2fa0
Gazrahel <gonzalo.olguin@gmail.com>
2025-06-21 12:30:47 -0400

    Create Gazrahel.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Gazrahel.py
@@ -0,0 +1,13 @@
+#la web oficial es https://www.python.org/
+
+#comentario
+"""otro comentario
+totalmente incomprendido
+:D
+"""
+'''esto tambien
+es u
+ti'''
+
+la_variable_loca = "esto es una variable"
+la_variable_loca = "esto es el nuevo valor de la variable"
